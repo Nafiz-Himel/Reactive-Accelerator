daac32cce25173d2f22f729cd0e551c0952a3999
Nafiz Himel <nafizhimel7@gmail.com>
2026-09-18 19:37:31 +0600

    Using filter

--- 01- Getting started with React- Describing the UI/1.10 Rendering Lists/src/App.jsx
+++ 01- Getting started with React- Describing the UI/1.10 Rendering Lists/src/App.jsx
@@ -1,16 +1,31 @@
-const people = [
-  'Creola Katherine Johnson: mathematician',
-  'Mario José Molina-Pasquel Henríquez: chemist',
-  'Mohammad Abdus Salam: physicist',
-  'Percy Lavon Julian: chemist',
-  'Subrahmanyan Chandrasekhar: astrophysicist'
-];
+const people = [{
+  id: 0,
+  name: 'Creola Katherine Johnson',
+  profession: 'mathematician',
+}, {
+  id: 1,
+  name: 'Mario José Molina-Pasquel Henríquez',
+  profession: 'chemist',
+}, {
+  id: 2,
+  name: 'Mohammad Abdus Salam',
+  profession: 'physicist',
+}, {
+  id: 3,
+  name: 'Percy Lavon Julian',
+  profession: 'chemist',
+}, {
+  id: 4,
+  name: 'Subrahmanyan Chandrasekhar',
+  profession: 'astrophysicist',
+}];
 
 export default function App() {
-  const listItems = people.map(person => <li>{person}</li>)
+  const chemists = people.filter(person => person.profession == "chemist").map(chemist => <li>{chemist.name}</li>)
+  // const listItems = chemists.map(chemist => <li>{chemist.name}</li>)
   return (
     <div>
-      <ul>{listItems}</ul>
+      <ul>{chemists}</ul>
     </div>
   );
 }
