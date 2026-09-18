3a1cc9f35d7f169cf3191e36f20c5c225e37644d
Nafiz Himel <nafizhimel7@gmail.com>
2026-09-18 08:23:48 +0600

    1.7 JavaScript in JSX with Curly Braces

--- 01- Getting started with React- Describing the UI/1.7 JavaScript in JSX with Curly Braces/src/components/Test.jsx
+++ 01- Getting started with React- Describing the UI/1.7 JavaScript in JSX with Curly Braces/src/components/Test.jsx
@@ -1,25 +1,21 @@
-const person = {
-  name: 'Gregorio Y. Zara',
-  theme: {
-    backgroundColor: 'black',
-    color: 'pink'
-  }
-};
+const style1 = {
+    background: "red"
+}
+
+const style2 = {
+    background : "green"
+}
+
+const color = "green"
 
 export default function Test() {
-  return (
-    <div style={person.theme}>
-      <h1>{person.name}'s Todos</h1>
-      <img
-        className="avatar"
-        src="https://react.dev/images/docs/scientists/7vQD0fPs.jpg"
-        alt="Gregorio Y. Zara"
-      />
-      <ul>
+    return (
+      <ul style={
+        color === "green" ? style1 : style2
+      }>
         <li>Improve the videophone</li>
         <li>Prepare aeronautics lectures</li>
         <li>Work on the alcohol-fuelled engine</li>
       </ul>
-    </div>
   );
 }
