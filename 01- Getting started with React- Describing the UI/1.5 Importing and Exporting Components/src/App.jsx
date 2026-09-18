9f6c4e3792758fa7f0a44f8ca16d26e67170ada0
Nafiz Himel <nafizhimel7@gmail.com>
2026-09-18 06:43:18 +0600

    1.5-Importing and Exporting Components

---
+++ 01- Getting started with React- Describing the UI/1.5 Importing and Exporting Components/src/App.jsx
@@ -0,0 +1,13 @@
+import Gallery, { Card as Image, Profile } from "./components/Gallery";
+
+function App() {
+  return (
+    <div>
+      <Profile />
+      <Gallery />
+      <Image />
+    </div>
+  );
+}
+
+export default App;
