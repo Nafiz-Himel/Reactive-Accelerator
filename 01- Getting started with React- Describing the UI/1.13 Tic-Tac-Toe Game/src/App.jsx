e12cd7b3aad0df407c5f667b314b7ee8516a33b7
Nafiz Himel <nafizhimel7@gmail.com>
2026-09-19 06:11:53 +0600

    Adding intereactivity

--- 01- Getting started with React- Describing the UI/1.13 Tic-Tac-Toe Game/src/App.jsx
+++ 01- Getting started with React- Describing the UI/1.13 Tic-Tac-Toe Game/src/App.jsx
@@ -2,31 +2,45 @@
 //  -> Board
 //       -> Square
 //  -> History
+import {useSate} from "react"
 
-function Square({value}){
-  return <button className="bg-white border border-grey-400 h-12 w-12 m-1 leading-9 text-lg">
-          {value}
-        </button>
+
+function Square() {
+  const [value, setvalue] = useState(null)
+
+  funciton handleClick(){
+    console.log("Clicked!")
+    setvalue("X")
+  }
+
+  return (
+    <button 
+      className="bg-white border border-grey-400 h-12 w-12 m-1 leading-9 text-lg"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
   return (
     <>
-    <div className="flex">
-      <Square />
-      <Square />
-      <Square />
-    </div>
-    <div className="flex">
-      <Square value = "X"/>
-      <Square />
-      <Square />
-    </div>
-    <div className="flex">
-      <Square />
-      <Square />
-      <Square />
-    </div>
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="flex">
+        <Square value="X" />
+        <Square />
+        <Square />
+      </div>
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </>
   );
 }
