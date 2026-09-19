86c86369d0fbc0f820ee33c895568a1b70dfc4ad
Nafiz Himel <nafizhimel7@gmail.com>
2026-09-19 07:07:56 +0600

    Calculating Winner

--- 01- Getting started with React- Describing the UI/1.13 Tic-Tac-Toe Game/src/App.jsx
+++ 01- Getting started with React- Describing the UI/1.13 Tic-Tac-Toe Game/src/App.jsx
@@ -25,15 +25,23 @@ function Square({value,onSquareClick}) {
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null))
-
   const [xIsNext,setXISNext] = useState(true)
+
+  const winner = calculateWinner(squares)
+  let status
+
+  if(winner){
+    status = `Winner: ${winner}`
+  }else{
+    status = `Next Player:` + (xIsNext ? " X" : " O")
+  }
 
   function handleClick(idx){
 
-    if(squares[idx]){
+    if(squares[idx] || calculateWinner(squares)){
       return
     }
-    
+
     console.log("Clicked!")
     
     // squares[0]="X"
@@ -52,6 +60,7 @@ export default function Board() {
   }
   return (
     <>
+      <div>{status}</div>
       <div className="flex">
         <Square value ={squares[0]} onSquareClick={() => handleClick(0)}/>
         <Square value ={squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -70,3 +79,25 @@ export default function Board() {
     </>
   );
 }
+
+
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
