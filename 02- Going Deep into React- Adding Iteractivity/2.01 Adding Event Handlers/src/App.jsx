57894e03d0166861d02089c23c7a3822354673cf
Nafiz Himel <nafizhimel7@gmail.com>
2026-09-23 11:07:43 +0600

    Passing event handlers as props

--- 02- Going Deep into React- Adding Iteractivity/2.01 Adding Event Handlers/src/App.jsx
+++ 02- Going Deep into React- Adding Iteractivity/2.01 Adding Event Handlers/src/App.jsx
@@ -1,20 +1,36 @@
-function AlertButton({ message, children }) {
+function Button({ onClick, children }) {
   return (
-    <button onClick={() => alert(message)}>
+    <button onClick={onClick}>
       {children}
     </button>
+  );
+}
+
+function PlayButton({ movieName }) {
+  function handlePlayClick() {
+    alert(`Playing ${movieName}!`);
+  }
+
+  return (
+    <Button onClick={handlePlayClick}>
+      Play "{movieName}"
+    </Button>
+  );
+}
+
+function UploadButton() {
+  return (
+    <Button onClick={() => alert('Uploading!')}>
+      Upload Image
+    </Button>
   );
 }
 
 export default function Toolbar() {
   return (
     <div>
-      <AlertButton message="Playing!">
-        Play Movie
-      </AlertButton>
-      <AlertButton message="Uploading!">
-        Upload Image
-      </AlertButton>
+      <PlayButton movieName="Kiki's Delivery Service" />
+      <UploadButton />
     </div>
   );
 }
